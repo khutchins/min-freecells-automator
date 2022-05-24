e71f5443cb985907a6e876ecc1fb4eb4ee05f21c
Kevin Hutchins <possessedcow@gmail.com>
2022-05-23 19:46:17 -0700

    Add analyzer.

--- Program.cs
+++ Program.cs
@@ -3,10 +3,12 @@ using System.Text;
 
 const bool verbose = false;
 const int DEFAULT_ITERATION_COUNT = 200000;
+const string FC_SOLVER_BIN_PATH = "T:/Apps/Freecell Solver 6.6.0/bin";
+const string SOLUTIONS_ROOT_PATH = "K:/git/min-freecells";
 
 static string DealFor(int gameNum) {
     Process process = new Process();
-    process.StartInfo.FileName = "T:/Apps/Freecell Solver 6.6.0/bin/pi-make-microsoft-freecell-board.exe";
+    process.StartInfo.FileName = $"{FC_SOLVER_BIN_PATH}/pi-make-microsoft-freecell-board.exe";
     process.StartInfo.Arguments = gameNum.ToString();
     process.StartInfo.RedirectStandardOutput = true;
     process.StartInfo.UseShellExecute = false;
@@ -16,10 +18,11 @@ static string DealFor(int gameNum) {
     return deal;
 }
 
-static string TryGame(string deal, int numFreeCells, int maxIterations) {
+static string TryGame(string dealPath, int numFreeCells, int maxIterations) {
     Process process = new Process();
-    process.StartInfo.FileName = "T:/Apps/Freecell Solver 6.6.0/bin/fc-solve.exe";
-    process.StartInfo.Arguments = $"-m -sn -l looking-glass -mi {maxIterations} --freecells-num {numFreeCells} \"T:/Apps/Freecell Solver 6.6.0/Scratch/scratch.txt\"";
+    process.StartInfo.FileName = $"{FC_SOLVER_BIN_PATH}/fc-solve.exe";
+    process.StartInfo.Arguments = $"-m -sn -l looking-glass -mi {maxIterations} --freecells-num {numFreeCells} \"{dealPath}\"";
+    //process.StartInfo.Arguments = $"-p -t -sam -l looking-glass -mi {maxIterations} --freecells-num {numFreeCells} \"{dealPath}\"";
     process.StartInfo.RedirectStandardOutput = true;
     process.StartInfo.UseShellExecute = false;
     process.Start();
@@ -31,6 +34,10 @@ static string TryGame(string deal, int numFreeCells, int maxIterations) {
     string[] splitted = output.Split("\r\n");
     int takeCount = splitted.Length - 6;
     string solution = string.Join("\n", splitted.Skip(2).SkipLast(4));
+    if (verbose) {
+        Console.WriteLine($"Solved with {numFreeCells} cells.");
+        Console.WriteLine(solution);
+    }
 
     return solution;
 }
@@ -48,7 +55,7 @@ static string WriteToFile(string path, string fileName, string contents) {
 }
 
 static string WriteDeal(int gameNum) {
-    return WriteToFile("T:/Apps/Freecell Solver 6.6.0/Scratch/", "scratch.txt", DealFor(gameNum));
+    return WriteToFile(SOLUTIONS_ROOT_PATH, "scratch.txt", DealFor(gameNum));
 }
 
 static int HandleGame(int gameNum, int maxIterations) {
@@ -76,7 +83,16 @@ static int HandleGame(int gameNum, int maxIterations) {
 
 static void WriteSolution(int gameNum, int fcCount, string solution) {
     int bucket = gameNum / 1000 * 1000;
-    WriteToFile($"T:/Apps/Freecell Solver 6.6.0/Scratch/Solve/{bucket}", $"{gameNum}.txt", $"{fcCount}\n{solution}");
+    WriteToFile($"{SOLUTIONS_ROOT_PATH}/Proofs/{bucket}", $"{gameNum}.txt", $"{fcCount}\n{solution}");
+}
+
+static string PathForGameNum(int gameNum) {
+    int bucket = gameNum / 1000 * 1000;
+    return $"{SOLUTIONS_ROOT_PATH}/Proofs/{bucket}/{gameNum}.txt";
+}
+
+static void CleanupScratch() {
+    File.Delete(Path.Combine(SOLUTIONS_ROOT_PATH, "scratch.txt"));
 }
 
 static void RunRange(int start, int endInclusive) {
@@ -90,11 +106,13 @@ static void RunRange(int start, int endInclusive) {
         }
         HandleGame(i, DEFAULT_ITERATION_COUNT);
     }
+    CleanupScratch();
     Console.WriteLine($"Finished deals {stopWatch?.ElapsedMilliseconds / 1000} seconds after last message.");
 }
 
 static void RunSingle(int num) {
     HandleGame(num, DEFAULT_ITERATION_COUNT);
+    CleanupScratch();
 }
 
 static void RunSelected(params int[] selected) {
@@ -102,6 +120,32 @@ static void RunSelected(params int[] selected) {
         int num = HandleGame(selected[i], DEFAULT_ITERATION_COUNT);
         if (verbose) Console.WriteLine($"Finished {selected[i]} with {num} free cells.");
     }
+    CleanupScratch();
 }
 
-RunRange(346207, 1000000);
+//RunSingle(46);
+//RunRange(50001, 200000);
+Analyze(1, 200000);
+
+static void Analyze(int start, int end) {
+    Stopwatch watch = new Stopwatch();
+    Dictionary<int, int> map = new Dictionary<int, int>();
+    for (int i = start; i <= end; i++) {
+        string first = File.ReadLines(PathForGameNum(i)).First();
+        int cellCount = int.Parse(first);
+        if (!map.ContainsKey(cellCount)) {
+            map[cellCount] = 0;
+        }
+        map[cellCount]++;
+        if (i % 10000 == 0) {
+            Console.WriteLine($"On deal {i}");
+        }
+    }
+
+    watch.Stop();
+    Console.WriteLine($"Analyzed {end - start + 1} deals in {watch.ElapsedMilliseconds / 1000}s.");
+    Console.WriteLine("Cell counts:");
+    foreach (var kvp in map) {
+        Console.WriteLine($"Cells: {kvp.Key} Number of Deals: {kvp.Value}");
+    }
+}
